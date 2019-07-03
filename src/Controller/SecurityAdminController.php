19cf795d2c19d4dfe43eb68973d7dffe23cb7dee
ClowClow <c.servieres@simplon-charleville.fr>
2019-07-03 10:51:22 +0200

    Ajout du formulaire d'inscription

--- src/Controller/SecurityAdminController.php
+++ src/Controller/SecurityAdminController.php
@@ -7,8 +7,23 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
+
 class SecurityAdminController extends AbstractController
 {
+  /**
+  * @Route("/Inscription", name="security_registration")
+  */
+  public function registration() {
+    $user = new User();
+
+    $form = $this->createForm(RegistrationType::class, $user);
+
+    return $this->render('security/registration.html.twig', [
+      'form' => $form->createView()
+    ]);
+  }
     /**
      * @Route("/login", name="app_login")
      */
