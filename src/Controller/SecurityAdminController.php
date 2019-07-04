f4bc990134148a591fb432c9e5cc9faa67f33024
ClowClow <c.servieres@simplon-charleville.fr>
2019-07-04 09:43:44 +0200

    petit bug solutionné

--- src/Controller/SecurityAdminController.php
+++ src/Controller/SecurityAdminController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -18,7 +19,8 @@ class SecurityAdminController extends AbstractController
   /**
   * @Route("/Inscription", name="security_registration")
   */
-  public function registration(Request $request, ObjectManager $manager) {
+  public function registration(Request $request, ObjectManager $manager,
+  UserPasswordEncoderInterface $encoder) {
     $user = new User();
 
     $form = $this->createForm(RegistrationType::class, $user);
@@ -26,6 +28,10 @@ class SecurityAdminController extends AbstractController
     $form->handleRequest($request);
 
     if ($form->isSubmitted() && $form->isValid()) {
+      $hash = $encoder->encodePassword($user, $user->getPassword());
+
+      $user->setPassword($hash);
+      
       $manager->persist($user);
       $manager->flush();
     }
