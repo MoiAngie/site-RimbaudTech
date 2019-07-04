f5109ab4918983f63b15fb6f62c9031c75579bc9
ClowClow <c.servieres@simplon-charleville.fr>
2019-07-04 09:56:48 +0200

    Encodage des mdp pour la base de donnée ok

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,11 @@ namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Security\Core\User\UserInterface;
 /**
  * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id()
@@ -75,5 +76,13 @@ class User
         $this->password = $password;
 
         return $this;
+    }
+
+    public function eraseCredentials() {}
+
+    public function getSalt() {}
+
+    public function getRoles() {
+      return ['ROLE_USER'];
     }
 }
