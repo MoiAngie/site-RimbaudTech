ee1317c0af91dddcbbf9d6d436f14d434548cf22
ClowClow <c.servieres@simplon-charleville.fr>
2019-07-08 10:28:46 +0200

    changement du controller

---
+++ src/Controller/AdminController.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
+
+use App\Entity\Articles;
+use App\Repository\ArticlesRepository;
+use App\Entity\Utilisateurs;
+use App\Repository\UtilisateursRepository;
+use App\Entity\Contact;
+use App\Form\ContactType;
+class AdminController extends AbstractController
+{
+  /**
+   * @Route("/rt/admin/homeAdmin", name="homeAdmin")
+   * ACCUEIL DU PANNEAU D'ADMINISTRATION
+   */
+  public function homeAdmin()
+  {
+      return $this->render('rt/admin/homeAdmin.html.twig', [
+      ]);
+  }
+
+  /**
+   * @Route("/rt/admin/portail-incubes", name="portailIncubes")
+   * PAGE PORTAIL DES INCUBES (page admin)
+   */
+  public function portailIncubes()
+  {
+      return $this->render('rt/admin/portail-incubes.html.twig', [
+      ]);
+  }
+
+  /**
+   * @Route("/rt/admin/create-incube", name="create-incube")
+   * PAGE AJOUT D'UN INCUBE (page admin)
+   */
+  public function createincube(Request $request, ObjectManager $manager)
+  {
+      $incube = new Utilisateurs;
+
+      $form = $this->createFormBuilder($incube)
+                    ->add ('name')
+                    ->add ('firstname')
+                    ->add ('company')
+                    ->add ('status')
+                    ->add ('profilpicture')
+                    ->add ('image1')
+                    ->add ('image2')
+                    ->add ('image3')
+                    ->add ('image4')
+                    ->add ('description')
+                    ->add ('comment')
+                    ->getForm();
+
+      $form->handleRequest($request);
+
+      if($form->isSubmitted() && $form->isValid()){
+
+      $manager->persist($incube);
+      $manager->flush();
+
+      return $this ->redirectToRoute('validation');
+      }
+
+      return $this->render('rt/admin/create-incube.html.twig', [
+        'formIncube' => $form->createView()
+      ]);
+  }
+
+  /**
+   * @Route("/rt/amin/portail-coworker", name="portailCoworker")
+   * PAGE PORTAIL COWORKER
+   */
+  public function portailCoworker()
+  {
+      return $this->render('rt/admin/portail-coworker.html.twig', [
+      ]);
+  }
+
+  /**
+   * @Route("/rt/amin/create-cowork", name="create-cowork")
+   * PAGE AJOUT COWORKER (page admin)
+   */
+  public function createcowork(Request $request, ObjectManager $manager)
+  {
+    $coworker = new Utilisateurs;
+
+    $form = $this->createFormBuilder($coworker)
+                  ->add ('name')
+                  ->add ('firstname')
+                  ->add ('company')
+                  ->add ('status')
+                  ->add ('profilpicture')
+                  ->add ('image1')
+                  ->add ('image2')
+                  ->add ('image3')
+                  ->add ('image4')
+                  ->add ('description')
+                  ->add ('comment')
+                  ->getForm();
+
+    $form->handleRequest($request);
+
+    if($form->isSubmitted() && $form->isValid()){
+
+    $manager->persist($coworker);
+    $manager->flush();
+
+    return $this ->redirectToRoute('validation');
+    }
+
+    return $this->render('rt/admin/create-coworker.html.twig', [
+      'formCoworker' => $form->createView()
+    ]);
+  }
+
+  /**
+   * @Route("/rt/amin/portail-personnel", name="portailPersonnel")
+   * PAGE PORTAIL MEMBRES DE L'EQUIPE
+   */
+  public function portailPersonnel()
+  {
+      return $this->render('rt/admin/portail-personnel.html.twig', [
+      ]);
+  }
+
+  /**
+   * @Route("/rt/admin/createPersonnel", name="createPersonnel")
+   * PAGE D'AJOUT D'UN MEMBRE DU PERSONNEL
+   */
+  public function createPersonnel()
+  {
+      return $this->render('rt/admin/create-personnel.html.twig', [
+      ]);
+  }
+
+  /**
+   * @Route("/rt/admin/portail-articles", name="portailArticles")
+   * PAGE PORTAIL ARTICLE
+   */
+  public function portailArticles()
+  {
+      return $this->render('rt/admin/portail-articles.html.twig', [
+      ]);
+  }
+
+  /**
+   * @Route("/rt/admin/create-article", name="create-article")
+   * PAGE DE CREATION D'UN ARTICLE (page admin)
+   */
+  public function createarticle(Request $request, ObjectManager $manager)
+  {
+      $article = new Articles;
+
+      $form = $this->createFormBuilder($article)
+                    ->add ('title')
+                    ->add ('author')
+                    ->add ('content')
+                    ->add ('image')
+                    ->getForm();
+
+      $form->handleRequest($request);
+
+      if($form->isSubmitted() && $form->isValid()){
+      $article->setCreatedAt(new \DateTime());
+
+      $manager->persist($article);
+      $manager->flush();
+
+      return $this ->redirectToRoute('validation');
+      }
+
+      return $this->render('rt/admin/create-article.html.twig', [
+        'formArticle' => $form->createView()
+      ]);
+  }
+
+  /**
+   * @Route("/rt/admin/validation", name="validation")
+   * PAGE TYPE ACTION REUSSIE
+   */
+  public function validation()
+  {
+      return $this->render('rt/admin/validation.html.twig', [
+      ]);
+  }
+
+  /**
+   * @Route("/rt/admin/portail-tarifs", name="portailTarifs")
+   * PAGE PORTAIL DES TARIFS
+   */
+  public function portailTarifs()
+  {
+      return $this->render('rt/admin/portail-tarifs.html.twig', [
+      ]);
+  }
+
+  /**
+   * @Route("/rt/admin/createTarif", name="createTarif")
+   * PAGE D'AJOUT TARIFS
+   */
+  public function createTarif()
+  {
+      return $this->render('rt/admin/create-tarif.html.twig', [
+      ]);
+  }
+
+  /**
+   * @Route("/rt/admin/createAccueil", name="createAccueil")
+   * PAGE DE MODIFICATION DE LA PAGE D'ACCUEIL
+   */
+  public function createAccueil()
+  {
+      return $this->render('rt/admin/create-accueil.html.twig', [
+      ]);
+  }
+}
--- src/Controller/RtController.php
+++ src/Controller/RtController.php
@@ -60,51 +60,6 @@ class RtController extends AbstractController
     }
 
     /**
-     * @Route("/rt/amin/create-cowork", name="create-cowork")
-     * PAGE AJOUT COWORKER (page admin)
-     */
-    public function createcowork(Request $request, ObjectManager $manager)
-    {
-      $coworker = new Utilisateurs;
-
-      $form = $this->createFormBuilder($coworker)
-                    ->add ('name')
-                    ->add ('firstname')
-                    ->add ('company')
-                    ->add ('status')
-                    ->add ('profilpicture')
-                    ->add ('image1')
-                    ->add ('image2')
-                    ->add ('image3')
-                    ->add ('image4')
-                    ->add ('description')
-                    ->add ('comment')
-                    ->getForm();
-
-      $form->handleRequest($request);
-
-      if($form->isSubmitted() && $form->isValid()){
-
-      $manager->persist($coworker);
-      $manager->flush();
-
-      return $this ->redirectToRoute('validation');
-      }
-
-      return $this->render('rt/admin/create-coworker.html.twig', [
-        'formCoworker' => $form->createView()
-      ]);
-    }
-    /**
-     * @Route("/rt/amin/portail-coworker", name="portailCoworker")
-     * PAGE PORTAIL COWORKER
-     */
-    public function portailCoworker()
-    {
-        return $this->render('rt/admin/portail-coworker.html.twig', [
-        ]);
-    }
-    /**
      * @Route("/rt/location", name="location")
      * PAGE LOCATION DE SALLE
      */
@@ -149,51 +104,6 @@ class RtController extends AbstractController
     }
 
     /**
-     * @Route("/rt/admin/create-incube", name="create-incube")
-     * PAGE AJOUT D'UN INCUBE (page admin)
-     */
-    public function createincube(Request $request, ObjectManager $manager)
-    {
-        $incube = new Utilisateurs;
-
-        $form = $this->createFormBuilder($incube)
-                      ->add ('name')
-                      ->add ('firstname')
-                      ->add ('company')
-                      ->add ('status')
-                      ->add ('profilpicture')
-                      ->add ('image1')
-                      ->add ('image2')
-                      ->add ('image3')
-                      ->add ('image4')
-                      ->add ('description')
-                      ->add ('comment')
-                      ->getForm();
-
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()){
-
-        $manager->persist($incube);
-        $manager->flush();
-
-        return $this ->redirectToRoute('validation');
-        }
-
-        return $this->render('rt/admin/create-incube.html.twig', [
-          'formIncube' => $form->createView()
-        ]);
-    }
-    /**
-     * @Route("/rt/admin/portail-incubes", name="portailIncubes")
-     * PAGE PORTAIL DES INCUBES (page admin)
-     */
-    public function portailIncubes()
-    {
-        return $this->render('rt/admin/portail-incubes.html.twig', [
-        ]);
-    }
-    /**
      * @Route("/rt/actu", name="actu")
      * PAGE ACTUALITES
      */
@@ -205,36 +115,7 @@ class RtController extends AbstractController
           'articles' =>$articles
         ]);
     }
-    /**
-     * @Route("/rt/admin/create-article", name="create-article")
-     * PAGE DE CREATION D'UN ARTICLE (page admin)
-     */
-    public function createarticle(Request $request, ObjectManager $manager)
-    {
-        $article = new Articles;
 
-        $form = $this->createFormBuilder($article)
-                      ->add ('title')
-                      ->add ('author')
-                      ->add ('content')
-                      ->add ('image')
-                      ->getForm();
-
-        $form->handleRequest($request);
-
-        if($form->isSubmitted() && $form->isValid()){
-        $article->setCreatedAt(new \DateTime());
-
-        $manager->persist($article);
-        $manager->flush();
-
-        return $this ->redirectToRoute('validation');
-        }
-
-        return $this->render('rt/admin/create-article.html.twig', [
-          'formArticle' => $form->createView()
-        ]);
-    }
     /**
      * @Route("/rt/article/{id}", name="article")
      * PAGE D'AFFICHAGE D'UN ARTICLE COMPLET
@@ -247,15 +128,7 @@ class RtController extends AbstractController
           'article' => $article
         ]);
     }
-    /**
-     * @Route("/rt/amin/portail-articles", name="portailArticles")
-     * PAGE PORTAIL ARTICLE
-     */
-    public function portailArticles()
-    {
-        return $this->render('rt/admin/portail-articles.html.twig', [
-        ]);
-    }
+
     /**
      * @Route("/rt/contact", name="contact")
      * PAGE CONTACT
@@ -268,69 +141,6 @@ class RtController extends AbstractController
 
         return $this->render('rt/contact.html.twig', [
           'formContact' => $formContact->createView()
-        ]);
-    }
-    /**
-     * @Route("/rt/admin/homeAdmin", name="homeAdmin")
-     * ACCUEIL DU PANNEAU D'ADMINISTRATION
-     */
-    public function homeAdmin()
-    {
-        return $this->render('rt/admin/homeAdmin.html.twig', [
-        ]);
-    }
-    /**
-     * @Route("/rt/admin/validation", name="validation")
-     * PAGE TYPE ACTION REUSSIE
-     */
-    public function validation()
-    {
-        return $this->render('rt/admin/validation.html.twig', [
-        ]);
-    }
-    /**
-     * @Route("/rt/admin/createTarif", name="createTarif")
-     * PAGE D'AJOUT TARIFS
-     */
-    public function createTarif()
-    {
-        return $this->render('rt/admin/create-tarif.html.twig', [
-        ]);
-    }
-    /**
-     * @Route("/rt/amin/portail-tarifs", name="portailTarifs")
-     * PAGE PORTAIL DES TARIFS
-     */
-    public function portailTarifs()
-    {
-        return $this->render('rt/admin/portail-tarifs.html.twig', [
-        ]);
-    }
-    /**
-     * @Route("/rt/admin/createPersonnel", name="createPersonnel")
-     * PAGE D'AJOUT D'UN MEMBRE DU PERSONNEL
-     */
-    public function createPersonnel()
-    {
-        return $this->render('rt/admin/create-personnel.html.twig', [
-        ]);
-    }
-    /**
-     * @Route("/rt/amin/portail-personnel", name="portailPersonnel")
-     * PAGE PORTAIL MEMBRES DE L'EQUIPE
-     */
-    public function portailPersonnel()
-    {
-        return $this->render('rt/admin/portail-personnel.html.twig', [
-        ]);
-    }
-    /**
-     * @Route("/rt/admin/createAccueil", name="createAccueil")
-     * PAGE DE MODIFICATION DE LA PAGE D'ACCUEIL 
-     */
-    public function createAccueil()
-    {
-        return $this->render('rt/admin/create-accueil.html.twig', [
         ]);
     }
 }
