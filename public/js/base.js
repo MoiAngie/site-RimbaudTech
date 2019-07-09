e2a26e4d5e386965b947bab9622996ac36beb05c
MoiAngie <a.maas@simplon-charleville.fr>
2019-07-09 16:22:45 +0200

    actualités 3 articles

--- public/js/base.js
+++ public/js/base.js
@@ -30,3 +30,21 @@ document.addEventListener('DOMContentLoaded', function () {
       function closeModal() {
           document.getElementById("modal").style.top = "-780px";
       }
+
+
+/* pour le collapse des articles  */
+
+      var coll = document.getElementsByClassName("collapsible");
+      var i;
+
+      for (i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function() {
+          this.classList.toggle("active");
+          var content = this.nextElementSibling;
+          if (content.style.maxHeight){
+            content.style.maxHeight = null;
+          } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+          }
+        });
+      }
