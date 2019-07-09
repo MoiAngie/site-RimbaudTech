45f0288d8ed0eb71a620ef85259f3a73b3f22753
MoiAngie <a.maas@simplon-charleville.fr>
2019-07-09 12:17:21 +0200

    avec bdd articles

--- src/Entity/Articles.php
+++ src/Entity/Articles.php
@@ -47,7 +47,7 @@ class Articles
     private $sousTitre1;
 
     /**
-     * @ORM\Column(type="string", length=255, nullable=true)
+     * @ORM\Column(type="text", nullable=true)
      */
     private $paragraphe1;
 
