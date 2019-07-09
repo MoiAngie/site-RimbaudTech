00b1c58663ba07f4705574a9ffe8f0dc64efa796
ClowClow <c.servieres@simplon-charleville.fr>
2019-07-09 11:18:12 +0200

    Formulaire article 'required'=>false ajoutés

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -20,15 +20,33 @@ class ArticleType extends AbstractType
           ->add('title', TextType::class)
           ->add('author', TextType::class)
           ->add ('image', FileType::class)
-          ->add('introduction', TextareaType::class)
-          ->add('sousTitre1', TextType::class)
-          ->add('paragraphe1', TextareaType::class)
-          ->add('sousTitre2', TextType::class)
-          ->add('paragraphe2', TextareaType::class)
-          ->add('sousTitre3', TextType::class)
-          ->add('paragraphe3', TextareaType::class)
-          ->add('sousTitre4', TextType::class)
-          ->add('paragraphe4', TextareaType::class)
+          ->add('introduction', TextareaType::class, [
+            'required' => false,
+          ])
+          ->add('sousTitre1', TextType::class, [
+            'required' => false,
+          ])
+          ->add('paragraphe1', TextareaType::class, [
+            'required' => false,
+          ])
+          ->add('sousTitre2', TextType::class, [
+            'required' => false,
+          ])
+          ->add('paragraphe2', TextareaType::class, [
+            'required' => false,
+          ])
+          ->add('sousTitre3', TextType::class, [
+            'required' => false,
+          ])
+          ->add('paragraphe3', TextareaType::class, [
+            'required' => false,
+          ])
+          ->add('sousTitre4', TextType::class, [
+            'required' => false,
+          ])
+          ->add('paragraphe4', TextareaType::class, [
+            'required' => false,
+          ])
       ;
   }
 
