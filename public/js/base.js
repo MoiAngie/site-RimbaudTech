9e55631b94531721db43e31b3bb7d62b1937bb1e
MoiAngie <a.maas@simplon-charleville.fr>
2019-07-29 16:11:48 +0200

    navbar en responsiv

--- public/js/base.js
+++ public/js/base.js
@@ -1,13 +1,9 @@
-
-
-
-
       /* Fonction qui permet l'apparition du bouton responsive de la navbar */
       function myFunction() {
         var x = document.getElementById("myTopnav");
-        if (x.className === "topnav") {
+        if (x.className === "topnav" ) {
           x.className += " responsive";
-        } else {
+        }else {
           x.className = "topnav";
         }
       }
